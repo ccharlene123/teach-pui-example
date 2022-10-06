e75d8ea186b239ba5b7896ef6826e7f0a330fe85
ccharlene123 <112356240+ccharlene123@users.noreply.github.com>
2022-10-06 15:45:20 -0400

    done??????

--- solution-hw5/js/cart.js
+++ solution-hw5/js/cart.js
@@ -60,7 +60,7 @@ function createCart(data) {
         // finalPrice = calculatePrice(cart[i].basePrice, glazingPrice[cart[i].glazing], priceAdaptation[cart[i].size]); 
         // clone.querySelector(".price").innerText = "$" + finalPrice.toFixed(2);
         
-        clone.querySelector(".product-thumbnails").src = "../assets/" + cartObject.type + "-cinnamon-roll.jpg";
+        clone.querySelector(".product-thumbnails").src = "../assets/" + rolls[cartObject.type].imageFile;
         clone.querySelector(".rollName").innerText = cartObject.type + " Cinnamon Roll";
         clone.querySelector(".glazeType").innerText = "Glazing: " + cartObject.glazing;
         clone.querySelector(".packSize").innerText = "Pack Size: " + cartObject.size;
