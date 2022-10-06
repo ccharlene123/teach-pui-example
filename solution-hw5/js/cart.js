0e39676bbd914ea316f45ce63f2a63f3983d853f
ccharlene123 <112356240+ccharlene123@users.noreply.github.com>
2022-10-06 12:43:33 -0400

    before final price calc

--- solution-hw5/js/cart.js
+++ solution-hw5/js/cart.js
@@ -7,17 +7,25 @@ class Roll {
     }
 }
 
-cart = [];
+// const cart = [];
+const cart = new Set();
 
 let rollOne = new Roll('Original', 'Sugar Milk', 1, 2.49);
 let rollTwo = new Roll('Walnut', 'Vanilla Milk', 12, 3.49);
 let rollThree = new Roll('Raisin', 'Sugar Milk', 3, 2.99);
 let rollFour = new Roll('Apple', 'Keep Original', 3, 3.49);
 
-cart.push(rollOne);
-cart.push(rollTwo);
-cart.push(rollThree);
-cart.push(rollFour);
+// arrays
+// cart.push(rollOne);
+// cart.push(rollTwo);
+// cart.push(rollThree);
+// cart.push(rollFour);
+
+// set
+cart.add(rollOne);
+cart.add(rollTwo);
+cart.add(rollThree);
+cart.add(rollFour);
 
 let priceAdaptation = {
     1:1,
@@ -38,35 +46,58 @@ function calculatePrice(basePrice, glazingPrice, packPrice) {
 }
 
 function createCart(data) {
-    for(let i=0; i<cart.length; i++){
-        let cartItem = document.getElementsByTagName("template")[0];
-        let cartItemClone = cartItem.content.cloneNode(true);
-        cartItemClone.querySelector(".rollName").innerText = cart[i].type + " Cinnamon Roll";
-        cartItemClone.querySelector(".glazeType").innerText = "Glazing: " + cart[i].glazing;
-        cartItemClone.querySelector(".packSize").innerText = "Pack Size: " + cart[i].size;
-        finalPrice = calculatePrice(cart[i].basePrice, glazingPrice[cart[i].glazing], priceAdaptation[cart[i].size]); 
-        cartItemClone.querySelector(".price").innerText = "$" + finalPrice.toFixed(2)
+    // for(let i=0; i<cart.length; i++){
+    for (let cartObject of cart) {
+        let cartElement = document.getElementById("ItemOne");
+        let docFragment = cartElement.content.cloneNode(true);
+
+        // console.log(clone);
+        // break;
+
+        let clone = docFragment.querySelector('.product-one');
+
+        // ARRAY SYNTAX
+        // clone.querySelector(".product-thumbnails").src = "../assets/" + cart[i].type + "-cinnamon-roll.jpg";
+        // clone.querySelector(".rollName").innerText = cart[i].type + " Cinnamon Roll";
+        // clone.querySelector(".glazeType").innerText = "Glazing: " + cart[i].glazing;
+        // clone.querySelector(".packSize").innerText = "Pack Size: " + cart[i].size;
+        // finalPrice = calculatePrice(cart[i].basePrice, glazingPrice[cart[i].glazing], priceAdaptation[cart[i].size]); 
+        // clone.querySelector(".price").innerText = "$" + finalPrice.toFixed(2);
         
-        document.querySelector(".main-cart").appendChild(cartItemClone);
+        clone.querySelector(".product-thumbnails").src = "../assets/" + cartObject.type + "-cinnamon-roll.jpg";
+        clone.querySelector(".rollName").innerText = cartObject.type + " Cinnamon Roll";
+        clone.querySelector(".glazeType").innerText = "Glazing: " + cartObject.glazing;
+        clone.querySelector(".packSize").innerText = "Pack Size: " + cartObject.size;
+        finalPrice = calculatePrice(cartObject.basePrice, glazingPrice[cartObject.glazing], priceAdaptation[cartObject.size]); 
+        clone.querySelector(".price").innerText = "$" + finalPrice.toFixed(2);
+        
+        const btnDelete = clone.querySelector('.remove');
+        console.log(btnDelete);
+
+        btnDelete.addEventListener('click', () => {
+            // HTML Element
+            clone.remove();
+
+            // Roll Object created in lines 13-16
+            cart.delete(cartObject);
+
+        });
+
+        document.querySelector(".main-cart").appendChild(clone);
+
+        
+        // console.log(clone);
+
     }
 }
+
 
 createCart(cart)
 console.log(cart)
 
-    // const btnDelete = cartItem.element.querySelector('.remove');
-    // console.log(btnDelete);
-    // btnDelete.addEventListener('click', () => {
-    // deleteNote(cartItem);
-    // });
+function deleteItem(clone) {
+    clone.remove();
+    cart.delete(clone);
+  }
 
-//calculate total cart price
-//change imgs
-//add remove function
-
-// function deleteItem(cartItem) {
-//     // remove the notecard DOM object from the UI
-//     cartItem.element.remove();
-//     // remove the actual Notecard object from our set of notecards
-//     cart.delete(cartItem);
-//   }
+//document.querySelector(".total-price").innerText = "$" + 
